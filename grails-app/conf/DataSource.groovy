307f5da973309ed8e79a784cb2c5082b4cf1286c
Roman Apostol <roman.apostol@gmail.com>
2012-04-22 11:09:16 +0300

    removed prod datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -12,30 +12,6 @@ environments {
     test {
     }
     production {
-        dataSource {
-            dbCreate = "none"
-            url = "jdbc:postgresql://localhost:5432/odin"
-            password = 'Sod1N0d1n'
-            username = "odin"
-            pooled = true
-            driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQLDialect
-            properties {
-                maxActive = 50
-                maxIdle = 25
-                minIdle = 10
-                initialSize = 5
-                minEvictableIdleTimeMillis = 60000
-                timeBetweenEvictionRunsMillis = 60000
-                numTestsPerEvictionRun = 3
-                maxWait = 10000
 
-                testOnBorrow = true
-                testWhileIdle = true
-                testOnReturn = false
-
-                validationQuery = "SELECT 1"
-            }
-        }
     }
 }
